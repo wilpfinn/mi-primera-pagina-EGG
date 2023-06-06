15cddc102661ad0791ced22588ad552062628f4a
William Finnemore <peter.intoxicado@gmail.com>
2023-06-05 22:09:56 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function showAlert(){
+    alert('Hola, esta es mi primera alerta desde JavaScript!');
+}
+
+
+
+
+document.querySelector("button.button-menu-toggle")
+    .addEventListener("click", function() {
+           document.querySelector(".nav-links").
+                      classList.toggle("nav-links-responsive")})
